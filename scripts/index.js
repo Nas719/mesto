bcd593453b2d528ea6feb5d1143da726b9e65bef
Anastasiia <naspolezhaeva@gmail.com>
2021-10-11 21:46:20 +0300

    refactored project

--- scripts/index.js
+++ scripts/index.js
@@ -5,44 +5,31 @@ const editButton = document.querySelector('.profile__edit-button');
 const formButton = document.querySelector('.popup__save-button');
 const form = document.querySelector('.popup__form');
 
-let name = document.querySelector('.profile__name').innerHTML;
-let job = document.querySelector('.profile__job').innerHTML;
+let name = document.querySelector('.profile__name').textContent;
+let job = document.querySelector('.profile__job').textContent;
 
 function openPopup () {
-   popup.classList.add('popup_isOpen')
+   popup.classList.add('popup_is-open')
    document.querySelector('.popup__input_type_name').value = name;
    document.querySelector('.popup__input_type_job').value = job;
 }
 
 function closePopup() {
-   popup.classList.remove('popup_isOpen')
+   popup.classList.remove('popup_is-open')
 
 }
-
-editButton.addEventListener('click', openPopup)
 
 popupCloseButton.addEventListener('click', closePopup)
-
-function popupClickHandler(event) {
-   console.log(event.target)
-
-   if (event.target.classList.contains('popup')) {
-     closePopup()
-   }
-}
-
-popup.addEventListener('click', popupClickHandler)
 
 function formSubmit(event) {
    event.preventDefault()
    closePopup();
    name = document.querySelector('.popup__input_type_name').value;
    job = document.querySelector('.popup__input_type_job').value;
-   document.querySelector('.profile__name').innerHTML = name;
-   document.querySelector('.profile__job').innerHTML = job;
+   document.querySelector('.profile__name').textContent = name;
+   document.querySelector('.profile__job').textContent = job;
 }
 
-formButton.addEventListener('click', function () {
-})
-
 form.addEventListener('submit', formSubmit);
+
+editButton.addEventListener('click', openPopup)
