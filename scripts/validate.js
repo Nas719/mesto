584a2056a8e00aecbf39bb7294325c658ede5ac4
Anastasiia <naspolezhaeva@gmail.com>
2021-11-03 16:43:16 +0300

    fix validate

--- scripts/validate.js
+++ scripts/validate.js
@@ -12,35 +12,40 @@ const hideInputError = (config, formElement, inputElement) => {
     spanError.classList.remove(config.spanErrorClass);
 }
 
-const checkInputValidity = (config, formElement, inputElement) => {
+const checkInputValidity = (config, formElement, inputElement, saveButton) => {
     if (!inputElement.validity.valid) {
         showInputError(config, formElement, inputElement);
     } else {
         hideInputError(config, formElement, inputElement);
     }
 
-    const saveButton = formElement.querySelector(config.submitButtonSelector);
     toggleButtonState(config, formElement, saveButton);
 }
 
-function setInputsEventListeners(config, formElement) {
+function setInputsEventListeners(config, formElement, saveButton) {
     const inputArray = Array.from(formElement.querySelectorAll(config.inputSelector));
 
     inputArray.forEach((inputElement) => {
         inputElement.addEventListener('input', () => {
-            checkInputValidity(config, formElement, inputElement);
-            toggleButtonState(formElement, saveButton);
+            checkInputValidity(config, formElement, inputElement, saveButton);
+            toggleButtonState(config, formElement, saveButton);
         });
     });
 }
 
 function enableValidation(config) {
     const formArray = Array.from(document.querySelectorAll(config.formSelector));
+
     formArray.forEach((formElement) => {
+        const saveButton = formElement.querySelector(config.submitButtonSelector);
         formElement.addEventListener('submit', (event) => {
             event.preventDefault();
+            if (formElement.id !== "popup-form-profile") {
+                formElement.reset();
+            }
+            toggleButtonState(config, formElement, saveButton);
         });
-        setInputsEventListeners(config, formElement);
+        setInputsEventListeners(config, formElement, saveButton);
     });
 }
 
@@ -58,6 +63,7 @@ enableValidation({
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__save-button',
+    closeButtonSelector: '.popup__close-button',
     inactiveButtonClass: 'popup__save-button_inactive',
     inputErrorClass: 'popup__input_type_error',
     spanErrorClass: 'popup__span-error_active',
